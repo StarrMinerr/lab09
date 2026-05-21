8b6b6e6a87a4ddc044ab2f4af19ebfc4d76da17c
106564353 <myles8338@gmail.com>
2026-05-21 17:56:25 +1000

    finished part1

---
+++ cars.php
@@ -0,0 +1,49 @@
+<?php
+require_once "settings.php";
+
+$dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+if ($dbconn) {
+
+    $query = "SELECT * FROM cars";
+    $result = mysqli_query($dbconn, $query);
+
+    if ($result) {
+
+        if (mysqli_num_rows($result) > 0) {
+
+            echo "<table border='1'>";
+            echo "<tr>
+                    <th>ID</th>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Price</th>
+                    <th>YOM</th>
+                  </tr>";
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['car_id'] . "</td>";
+                echo "<td>" . $row['make'] . "</td>";
+                echo "<td>" . $row['model'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['yom'] . "</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+
+        } else {
+            echo "<p>There are no cars to display.</p>";
+        }
+
+    } else {
+        echo "<p>Query failed.</p>";
+    }
+
+    mysqli_close($dbconn);
+
+} else {
+    echo "<p>Unable to connect to the database.</p>";
+}
+?>
